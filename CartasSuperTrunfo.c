b0a1309542cd17f7f7821618bfe3f2cc44163d6c
wagnercardoso18 <waguincardoso5678@gmail.com>
2025-04-10 20:13:35 +0000

    criei print e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,61 @@
 //Teste larissa
 
 int main() {
+    char codigo1[5], codigo2[5];
+    char estado1[100], estado2[100];
+    char cidade1[100], cidade2[100];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+
+    printf("Cadastro da primeira cidade:\n");
+    printf("Código: ");
+    scanf("%s", codigo1);
+
+    printf("Estado: ");
+    scanf(" %[^\n]", estado1);
+
+    printf("Cidade: ");
+    scanf(" %[^\n]", cidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em milhões): ");
+    scanf("%f", &pib1);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+    // Cadastro da segunda cidade
+    printf("\nCadastro da segunda cidade:\n");
+    printf("Código: ");
+    scanf("%s", codigo2);
+
+    printf("Estado: ");
+    scanf(" %[^\n]", estado2);
+
+    printf("Cidade: ");
+    scanf(" %[^\n]", cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em milhões): ");
+    scanf("%f", &pib2);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+    
+    
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
